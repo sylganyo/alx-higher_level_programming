3b1952a3367e963cbac195082e803835cc1b9e9a
sylganyo <sylvanusg22@yahoo.com>
2023-03-14 08:21:43 -0700

    1-create_database_if_missing.sql

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,7 +1,2 @@
-$ cat my_script.sql
--- 3 first students in the Batch ID=3
--- because Batch 3 is the best!
-SELECT id, name FROM students WHERE batch_id = 3 ORDER BY created_at DESC LIMIT 3;
-$
-
+-- This cript creates a Database by name hbtn_0c_0
 CREATE DATABASE hbtn_0c_0
